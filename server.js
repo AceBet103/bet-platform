a0b5b4e2a736f2e039e9daa4af14035671a58d9b
Christopher Rameau <abet97984@gmail.com>
2026-01-27 10:44:38 -0500

    fix server

--- server.js
+++ server.js
@@ -24,7 +24,13 @@ const admin = require("./middleware/admin");
 const app = express();
 
 /* ---------- GLOBAL MIDDLEWARE ---------- */
-app.use(cors({ origin: "http://localhost:3000" }));
+app.use(cors({
+  origin: [
+    "http://localhost:3000",
+    "https://bet-platform-pearl.vercel.app"
+  ]
+}));
+
 app.use(express.json());
 
 /* ---------- ROUTE REGISTRATION ---------- */
@@ -209,7 +215,7 @@ app.get("/api/events", async (req, res) => {
 });
 
 /* ---------- SERVER START ---------- */
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
-  console.log("Serveur lancé sur http://localhost:" + PORT);
+  console.log("Serveur lancé sur le port " + PORT);
 });
